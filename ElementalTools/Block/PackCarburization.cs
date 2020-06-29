2fc9b7c07eb0f43ac62a71030c1a48b4b36e7864
melchior <melchior@users.osdn.me>
2020-06-28 21:15:06 -0400

    W.I.P. Placeholders for Pack Carburization

---
+++ ElementalTools/Block/PackCarburization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Vintagestory.API.Common;
+
+namespace ElementalTools
+{
+	public class PackCarburization : Block
+	{
+		//Breakable Carburization 'box'; for Steel making.
+
+		//Recipie Options #1: Charcoal & Bonemeal & Blue-clay
+		//Recipie Options #2: Leather & Fat & Blue-clay
+
+		//Heat to 'Red' hot for ~ 60 minutes (equive to ??? game cook time ??? )
+	}
+}
+
---
+++ ElementalTools/BlockEntities/PackCarburizationEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Vintagestory.API.Common;
+
+namespace ElementalTools
+{
+	public class PackCarburizationEntity : BlockEntity
+	{
+		//Item Container for Iron object to convert...
+
+	}
+}
+
