2426d916d46baa49d70466b8616fb3afdf17d5f1
melchior <melchior@users.osdn.me>
2021-03-26 18:17:44 -0400

    W.I.P. almost OK collision detection

--- Assorted/BlockClasses/CollapsingBlock.cs
+++ Assorted/BlockClasses/CollapsingBlock.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
-using Vintagestory.GameContent;
+
 
 namespace FirstMachineAge
 {
 	public class CollapsingBlock : Block
 	{
-		private readonly Cuboidf[ ] collapseZones = { new Cuboidf(0.06f, 0.1f, 0.1f, 0.937f, 0.25f, 1.0f) };
+		private readonly Cuboidf[ ] collapseZones = { new Cuboidf(0.1f, 0.1f, 0.1f, 0.9f, 0.25f, 0.9f) };
 
 
 		private void MabeyCollapse(IWorldAccessor localAcc, BlockPos here, float delay)
@@ -24,22 +26,26 @@ namespace FirstMachineAge
 		//hitboxSize: { x: 0.6, y: 1.85 } > Seraph
 		//hitboxSize: { x: 0.85, y: 0.5 }> Pup
 		//hitboxSize: { x: 0.4, y: 0.3 } > lil'Bunny
-			 
-		bool enough = false;				
-		var victems = localAcc.GetIntersectingEntities(here.UpCopy( ), collapseZones, (e) => { return true;});//TOP 'Surface' - excluding edges
 
-		foreach (var entity in victems) 
-		{
-			if (entity.IsInteractable &&
-			entity.Properties.HitBoxSize.X >= 0.5 &&
-			entity.Properties.HitBoxSize.Y >= 0.5) {
-			#if DEBUG
-			ServerAPI.Logger.VerboseDebug($"Collision-check:  ({entity.Code}) is large enough to trigger COLLAPSE!");
-			#endif
-			enough = true;
-			break;
+		bool enough = false;
+		var victems = localAcc.GetIntersectingEntities(here.UpCopy( ), collapseZones, (e) => { return (e is EntityAgent); } );//TOP 'Surface' - excluding edges
+		if (victems.Length > 2) { enough = true; }
+		else {
+				foreach (var entity in victems) 
+				{
+					if (entity.IsInteractable &&
+					entity.Properties.HitBoxSize.X >= 0.51 &&
+					entity.Properties.HitBoxSize.Y >= 0.51) 
+					{
+					#if DEBUG
+					ServerAPI.Logger.VerboseDebug($"Collision box ( W{collapseZones[0].Width} H{collapseZones[0].Height} L{collapseZones[0].Length})");
+					ServerAPI.Logger.VerboseDebug($"Collision-check:  ({entity.Code}) is large enough to trigger COLLAPSE!");
+					#endif
+					enough = true;
+					break;
+					}
+				}
 			}
-		}
 
 		if (enough) {
 		localAcc.BlockAccessor.BreakBlock(here.Copy(), null);
@@ -85,6 +91,7 @@ namespace FirstMachineAge
 		public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
 		{
 		if (api.Side.IsClient( )) return;
+		if (!(entity is EntityAgent)) return;
 		//Start to shake...with particles , dust, creaking sounds
 		#if DEBUG
 		ServerAPI.Logger.VerboseDebug($"OnEntityCollide ({entity.Code}) of [{entity.GetType( ).Name}] @ {pos} impact: {isImpact}");
@@ -92,6 +99,19 @@ namespace FirstMachineAge
 
 		//Tick Callback; in 200ms...
 		ServerAPI.World.RegisterCallbackUnique(MabeyCollapse, pos.Copy() ,50);		
+		}
+
+		public override void OnEntityInside(IWorldAccessor world, Entity entity, BlockPos pos)
+		{
+		if (api.Side.IsClient( )) return;
+		if (!(entity is EntityAgent)) return;
+		//Start to shake...with particles , dust, creaking sounds
+		#if DEBUG
+		ServerAPI.Logger.VerboseDebug($"OnEntityInside ({entity.Code}) of [{entity.GetType( ).Name}] @ {pos} ");
+		#endif
+
+		//Tick Callback; in 200ms...
+		ServerAPI.World.RegisterCallbackUnique(MabeyCollapse, pos.Copy( ), 50);
 		}
 
 		public override string GetPlacedBlockName(IWorldAccessor world, BlockPos pos)
@@ -123,33 +143,23 @@ namespace FirstMachineAge
 		public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1)
 		{
 		if (api.Side.IsClient( )) {
-		//Dust
-		/*
-		byPlayer.Entity.World.SpawnParticles(new SimpleParticleProperties( ) {
-			MinQuantity = 0,
-			AddQuantity = 10,
-			Color = ColorUtil.ToRgba(128, 128, 128, 64),
-			MinPos = new Vec3d(posx + faceVec.X * 0.01f, posy + faceVec.Y * 0.01f, posz + faceVec.Z * 0.01f),
-			AddPos = new Vec3d(0, 0, 0),
-			MinVelocity = new Vec3f(
-				   4 * faceVec.X,
-				   4 * faceVec.Y,
-				   4 * faceVec.Z
-			   ),
-			AddVelocity = new Vec3f(
-				   8 * (( float )rnd.NextDouble( ) - 0.5f),
-				   8 * (( float )rnd.NextDouble( ) - 0.5f),
-				   8 * (( float )rnd.NextDouble( ) - 0.5f)
-			   ),
-			LifeLength = 0.025f,
-			GravityEffect = 0f,
-			MinSize = 0.03f,
-			MaxSize = 0.4f,
-			ParticleModel = EnumParticleModel.Quad,
-			VertexFlags = 200,
-			SizeEvolve = new EvolvingNatFloat(EnumTransformFunction.LINEAR, -0.15f)
-		}, byPlayer);
-		*/
+		//Bits of broken block
+
+		var particleProps = new SimpleParticleProperties(1, 3, this.GetRandomColor(api as ICoreClientAPI, pos.Copy( ), BlockFacing.UP), new Vec3d( ), new Vec3d( ), new Vec3f(-0.25f, -0.25f, -0.25f), new Vec3f(0.25f, 0.25f, 0.25f), 1, 1, 0.1f, 0.3f, EnumParticleModel.Quad);
+		particleProps.AddPos.Set(1.4, 1.4, 1.4);
+		particleProps.AddQuantity = 20;
+		particleProps.MinVelocity.Set(-0.25f, 0, -0.25f);
+		particleProps.AddVelocity.Set(0.5f, 1, 0.5f);
+		particleProps.WithTerrainCollision = true;
+		particleProps.ParticleModel = EnumParticleModel.Cube;
+		particleProps.LifeLength = 1.5f;
+		particleProps.SizeEvolve = EvolvingNatFloat.create(EnumTransformFunction.QUADRATIC, -0.5f);
+		particleProps.GravityEffect = 2.5f;
+		particleProps.MinSize = 0.5f;
+		particleProps.MaxSize = 1.5f;
+
+		byPlayer.Entity.World.SpawnParticles(particleProps, byPlayer);
+		
 		}
 
 		base.OnBlockBroken(world, pos, byPlayer, dropQuantityMultiplier);
